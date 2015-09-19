63ce9cd59812384624f1155754e8d74f73c6f61b
Jonathan Torres <jonathantorres41@gmail.com>
2015-09-19 13:28:09 -0400

    Refactor some helper to a default script helper.

---
+++ src/Helpers/Script.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace JonathanTorres\Construct\Helpers;
+
+class Script
+{
+    /**
+     * Do an initial composer install in constructed project.
+     *
+     * @param string $folder
+     *
+     * @return void
+     */
+    public function composerInstall($folder)
+    {
+        $command = 'cd ' . $folder . ' && composer install';
+
+        exec($command);
+    }
+
+    /**
+     * Generate default behat context.
+     *
+     * @param string $folder
+     *
+     * @return void
+     */
+    public function initBehat($folder)
+    {
+        $command = 'cd ' . $folder . ' && vendor/bin/behat --init';
+
+        exec($command);
+    }
+
+    /**
+     * Generate default codeception suites.
+     *
+     * @param string $folder
+     *
+     * @return void
+     */
+    public function bootstrapCodeception($folder)
+    {
+        $command = 'cd ' . $folder . ' && vendor/bin/codecept bootstrap';
+
+        exec($command);
+    }
+}
