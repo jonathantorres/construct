d6434451a6f32f25a86b2e99d375387b1e4942a2
Jonathan Torres <jonathantorres41@gmail.com>
2017-08-03 21:53:03 -0400

    Add test for settings.

--- tests/SettingsTest.php
+++ tests/SettingsTest.php
@@ -11,21 +11,23 @@ class SettingsTest extends TestCase
 
     protected function setUp()
     {
-        $this->settings = new Settings(
-            'jonathantorres/logger',
-            'phpunit',
-            'MIT',
-            'JonathanTorres\Logger',
-            true,
-            true,
-            'some, another, keyword',
-            false,
-            false,
-            '5.6.0',
-            true,
-            false,
-            null
-        );
+        $this->settings = new Settings();
+        $this->settings->setProjectName('jonathantorres/logger');
+        $this->settings->setTestingFramework('phpunit');
+        $this->settings->setLicense('MIT');
+        $this->settings->setNamespace('JonathanTorres\Logger');
+        $this->settings->setGitInit(true);
+        $this->settings->setPhpcsConfiguration(true);
+        $this->settings->setComposerKeywords('some, another, keyword');
+        $this->settings->setVagrantfile(false);
+        $this->settings->setEditorConfig(false);
+        $this->settings->setPhpVersion('7.0.0');
+        $this->settings->setEnvironmentFiles(true);
+        $this->settings->setLgtmConfiguration(false);
+        $this->settings->setGithubTemplates(false);
+        $this->settings->setGithubDocs(false);
+        $this->settings->setCodeOfConduct(false);
+        $this->settings->setCliFramework(null);
     }
 
     public function test_settings_are_retrieved()
@@ -39,10 +41,12 @@ class SettingsTest extends TestCase
         $this->assertSame('some, another, keyword', $this->settings->getComposerKeywords());
         $this->assertFalse($this->settings->withVagrantfile());
         $this->assertFalse($this->settings->withEditorConfig());
-        $this->assertSame('5.6.0', $this->settings->getPhpVersion());
+        $this->assertSame('7.0.0', $this->settings->getPhpVersion());
         $this->assertTrue($this->settings->withEnvironmentFiles());
         $this->assertFalse($this->settings->withLgtmConfiguration());
         $this->assertFalse($this->settings->withGithubDocs());
+        $this->assertFalse($this->settings->withGithubTemplates());
+        $this->assertFalse($this->settings->withCodeOfConduct());
         $this->assertNull($this->settings->getCliFramework());
     }
 
