94c20d1feea4a7d42948db3571c689125272d2c8
Jonathan Torres <jonathantorres41@gmail.com>
2017-08-21 22:17:51 -0400

    Add class to mantain current git ignores and git attributes.

---
+++ src/GitAttributes.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Construct;
+
+class GitAttributes
+{
+    /**
+     * The files to ignore on exporting.
+     *
+     * @var array
+     */
+    private $exportIgnores = [];
+
+    /**
+     * The directories and files to ignore in Git repositories.
+     *
+     * @var array
+     */
+    private $gitIgnores = ['/vendor', 'composer.lock'];
+
+    /**
+     * Get the export ignores
+     *
+     * @return array
+     */
+    public function getExportIgnores()
+    {
+        return $this->exportIgnores;
+    }
+
+    /**
+     * Add a file to the export ignores
+     *
+     * @param string $ignore
+     *
+     * @return  void
+     */
+    public function addIgnore($ignore)
+    {
+        $this->exportIgnores[] = $ignore;
+    }
+
+    /**
+     * Get the directories and files to ignore on git repositories.
+     *
+     * @return array
+     */
+    public function getGitIgnores()
+    {
+        return $this->gitIgnores;
+    }
+}
