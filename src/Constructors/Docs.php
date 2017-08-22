ff930862ad84a6ab3a4e9055bed631bfde0fcd6d
Jonathan Torres <jonathantorres41@gmail.com>
2017-08-21 22:18:19 -0400

    Add docs constructor.

---
+++ src/Constructors/Docs.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Construct\Constructors;
+
+class Docs extends Constructor implements ConstructorContract
+{
+    /**
+     * Adds README.md CONTRIBUTING.md and the CHANGELOG.md files.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->readme();
+        $this->contributing();
+        $this->changelog();
+    }
+
+    /**
+     * Generate README.md file.
+     *
+     * @return void
+     */
+    private function readme()
+    {
+        if ($this->settings->withCodeOfConduct() === false && $this->settings->withGithubTemplates() === false) {
+            $readme = $this->filesystem->get(__DIR__ . '/../stubs/README.stub');
+        } elseif ($this->settings->withCodeOfConduct() === false && $this->settings->
+            withGithubTemplates() === true) {
+            $readme = $this->filesystem->get(__DIR__ . '/../stubs/README.GITHUB.TEMPLATES.stub');
+        } elseif ($this->settings->withCodeOfConduct() === true && $this->settings->
+            withGithubTemplates() === false) {
+            $readme = $this->filesystem->get(__DIR__ . '/../stubs/README.CONDUCT.stub');
+        } else {
+            $readme = $this->filesystem->get(__DIR__ . '/../stubs/README.CONDUCT.GITHUB.TEMPLATES.stub');
+        }
+
+        $stubs = [
+            '{project_upper}',
+            '{license}',
+            '{vendor_lower}',
+            '{project_lower}'
+        ];
+
+        $values = [
+            $this->settings->getProjectUpper(),
+            $this->settings->getLicense(),
+            $this->settings->getVendorLower(),
+            $this->settings->getProjectLower()
+        ];
+
+        $content = str_replace($stubs, $values, $readme);
+
+        $this->filesystem->put($this->settings->getProjectLower() . '/' . 'README.md', $content);
+        $this->gitAttributes->addIgnore('README.md');
+    }
+
+    /**
+     * Generate CONTRIBUTING.md file.
+     *
+     * @return void
+     */
+    private function contributing()
+    {
+        if ($this->settings->withPhpcsConfiguration()) {
+            $contributing = $this->filesystem->get(__DIR__ . '/../stubs/CONTRIBUTING.PHPCS.stub');
+        } else {
+            $contributing = $this->filesystem->get(__DIR__ . '/../stubs/CONTRIBUTING.stub');
+        }
+
+        $placeholder = ['{project_lower}', '{git_message_path}'];
+        $replacements = [$this->settings->getProjectLower(), '.gitmessage'];
+
+        if ($this->settings->withGithubTemplates()) {
+            $replacements = [$this->settings->getProjectLower(), '../.gitmessage'];
+        }
+
+        $content = str_replace($placeholder, $replacements, $contributing);
+
+        $this->filesystem->put($this->settings->getProjectLower() . '/' . 'CONTRIBUTING.md', $content);
+        $this->gitAttributes->addIgnore('CONTRIBUTING.md');
+    }
+
+    /**
+     * Generate CHANGELOG.md file.
+     *
+     * @return void
+     */
+    protected function changelog()
+    {
+        $changelog = $this->filesystem->get(__DIR__ . '/../stubs/CHANGELOG.stub');
+        $content = str_replace(
+            '{creation_date}',
+            (new \DateTime())->format('Y-m-d'),
+            $changelog
+        );
+
+        $this->filesystem->put($this->settings->getProjectLower() . '/' . 'CHANGELOG.md', $content);
+        $this->gitAttributes->addIgnore('CHANGELOG.md');
+    }
+}
